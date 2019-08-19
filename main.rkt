816e009e783372541092a8a0db0dc86732dd0524
Michael Ballantyne <michael.ballantyne@gmail.com>
2019-08-19 13:01:30 -0400

    finish basic compiler

--- main.rkt
+++ main.rkt
@@ -4,10 +4,10 @@
   racket/list
   (only-in racket/cmdline parse-command-line)
   ee-lib/define
+  
   (for-syntax
    racket/base
    (rename-in syntax/parse [define/syntax-parse def/stx])
-   racket/syntax
    syntax/stx
    ee-lib
    racket/list
@@ -16,7 +16,11 @@
 
 (provide define/command-line-options
          choice multi
-         simple-argument checked-argument)
+         simple-argument checked-argument
+
+         switch/o list/o
+         numbered-flags/f
+         int-range/argt)
 
 (define-literal-forms cmdline-literals
   "command line option, flag, and argument specifiers cannot be used as expressions"
@@ -27,8 +31,9 @@
 
 (define-extensible-syntax option-syntax)
 (define-extensible-syntax flag-syntax)
-(define-extensible-syntax argument-syntax)
+(define-extensible-syntax argtype-syntax)
 
+; Expander
 (begin-for-syntax
   (define-syntax-class flag-names
     #:attributes [names]
@@ -43,31 +48,35 @@
              #:attr type #'simple-argument)
     (pattern [name:id type]))
   
-  (define/hygienic (expand-option stx) #:definition
+  (define/hygienic-metafunction (expand-option stx) #:definition
     (syntax-parse stx
       #:literal-sets (cmdline-literals)
       [(choice #:required flag ...+)
-       (def/stx (flag^ ...) (stx-map expand-flag #'(flag ...)))
-       (qstx/rc (choice #:required flag^ ...))]
-      [(choice #:default flag ...+)
-       (def/stx (flag^ ...) (stx-map expand-flag #'(flag ...)))
-       (qstx/rc (choice #:default flag^ ...))]
+       (qstx/rc (choice #:required (~@ . (expand-flag flag)) ...))]
+      [(choice #:default default-expr flag ...+)
+       (qstx/rc (choice #:default default-expr (~@ . (expand-flag flag)) ...))]
       [(multi init-expr flag ...+)
-       (def/stx (flag^ ...) (stx-map expand-flag #'(flag ...)))
-       (qstx/rc (multi init-expr flag^ ...))]))
+       (qstx/rc (multi init-expr (~@ . (expand-flag flag)) ...))]
+      [(head:id . rest)
+       #`(expand-option #,(option-syntax-transform (lookup #'head) this-syntax))]))
 
-  (define/hygienic (expand-flag stx) #:definition
+  (define/hygienic-metafunction (expand-flag stx) #:definition
     (syntax-parse stx
-      #:literal-sets (cmdline-literals)
+      #:literal-sets (cmdline-literals) #:literals (begin)
+      [(begin f ...)
+       (qstx/rc ((~@ . (expand-flag f)) ...))]
       [[names:flag-names arg:arg-spec ... desc:string e]
-       (def/stx (arg-type^ ...) (stx-map expand-argument-type #'(arg.type ...)))
-       (qstx/rc [names.names ([arg.name arg-type^] ...) desc e])]))
+       (qstx/rc ([names.names [arg.name (expand-argument-type arg.type)] ... desc e]))]
+      [(head:id . rest)
+       #`(expand-flag #,(flag-syntax-transform (lookup #'head) this-syntax))]))
 
-  (define/hygienic (expand-argument-type stx) #:definition
+  (define/hygienic-metafunction (expand-argument-type stx) #:definition
     (syntax-parse stx
       #:literal-sets (cmdline-literals)
       [simple-argument this-syntax]
-      [(checked-argument desc:string parser) this-syntax]))
+      [(checked-argument desc:string parser) this-syntax]
+      [(head:id . rest)
+       #`(expand-argument-type #,(argtype-syntax-transform (lookup #'head) this-syntax))]))
    
   (define/hygienic (expand-define/command-line-options stx) #:definition
     (syntax-parse stx
@@ -78,62 +87,58 @@
                 (~optional (~seq #:rest rest-arg:arg-spec)))
        #:with ([option-name:id opt-spec] ...) #'(~? (option-bindings ...) ())
        #:with (arg:arg-spec ...) #'(~? (positional-bindings ...) ())
-
-       (def/stx (expanded-opt-spec ...) (stx-map expand-option #'(opt-spec ...)))
-       (def/stx (expanded-arg-type ...) (stx-map expand-argument-type #'(arg.type ...)))
-       (def/stx rest
-         (and (attribute rest-arg)
-              (list #'rest-arg.name (expand-argument-type #'rest-arg.type))))
-       
        (qstx/rc
         (form-id (~? name-expr (find-system-path 'run-file))
                  (~? argv-expr (current-command-line-arguments))
-                 ([option-name expanded-opt-spec] ...)
-                 ([arg.name expanded-arg-type] ...)
-                 rest))])))
+                 ([option-name (expand-option opt-spec)] ...)
+                 ([arg.name (expand-argument-type arg.type)] ...)
+                 (~? [rest-arg.name (expand-argument-type rest-arg.type)] #f)))])))
 
+; runtime
+(define identity/p (lambda (x) x))
 (define unset
   (let () (struct unset ()) (unset)))
 
-(define identity/p (lambda (x) x))
-
 (define (make-finish-proc option-keys initial-option-values required-option-keys
                           arg-names arg-parsers maybe-parse-rest)
-  (define (f option-procs . args)
-    (define-values (positional-args rest-args) (split-at args (length arg-parsers)))
-   
-    (define option-map
-      (for/fold ([option-map (make-immutable-hash (map cons option-keys initial-option-values))])
-                ([option-proc option-procs])
-        (option-proc option-map)))
+  (procedure-reduce-arity
+   (lambda (option-procs . args)
+     (define-values (positional-args rest-args) (split-at args (length arg-parsers)))
 
-    (for ([(key flags) required-option-keys])
-      (when (eq? (hash-ref option-map key) unset)
-        (raise-user-error (format "one of these flags must be specified: ~a" flags))))
+     (define initial-option-map
+       (make-immutable-hash (map cons option-keys initial-option-values)))
+     (define final-option-map
+       (for/fold ([option-map initial-option-map])
+                 ([option-proc option-procs])
+         (option-proc option-map)))
+
+     (for ([(key flags) required-option-keys])
+       (when (eq? (hash-ref final-option-map key) unset)
+         (raise-user-error (format "one of these flags must be specified: ~a" flags))))
     
-    (define options
-      (for/list ([key option-keys])
-        (hash-ref option-map key)))
+     (define options
+       (for/list ([key option-keys])
+         (hash-ref final-option-map key)))
 
-    (define positionals
-      (for/list ([arg positional-args]
-                 [parse arg-parsers])
-        (parse arg)))
+     (define positionals
+       (for/list ([arg positional-args]
+                  [parse arg-parsers])
+         (parse arg)))
 
-    (define maybe-rest
-      (if maybe-parse-rest
-          (list (map maybe-parse-rest rest-args))
-          '()))
+     (define maybe-rest
+       (if maybe-parse-rest
+           (list (map maybe-parse-rest rest-args))
+           '()))
 
-    (apply values
-           (append options
-                   positionals
-                   maybe-rest)))
+     (apply values
+            (append options
+                    positionals
+                    maybe-rest)))
+   (if maybe-parse-rest
+       (arity-at-least (+ 1 (length arg-parsers)))
+       (+ 1 (length arg-parsers)))))
 
-  (if maybe-parse-rest
-      (procedure-reduce-arity f (arity-at-least (+ 1 (length arg-parsers))))
-      (procedure-reduce-arity f (+ 1 (length arg-parsers)))))
-
+; compiler
 (begin-for-syntax
   (define (identifier->string-literal id) #`#,(symbol->string (syntax-e id)))
   
@@ -147,26 +152,33 @@
       [(multi init-expr . _)
        #'init-expr]))
 
+  (define (arg-type-parser-expr arg-type-stx)
+    (syntax-parse arg-type-stx
+      #:literal-sets (cmdline-literals)
+      [simple-argument #'identity/p]
+      [(checked-argument desc parser) #'parser]))
+
   (define-syntax-class exp-flag-spec
     (pattern [[name:string ...] ([arg:id arg-spec] ...) desc:string e]))
+
+  (define ((compile-flag key type) flag-stx)
+    (syntax-parse flag-stx
+      [[[name:string ...] [arg:id arg-spec] ... desc:string e]
+       (def/stx (arg-p ...) (stx-map arg-type-parser-expr #'(arg-spec ...)))
+       (def/stx hash-op (case type [(choice) #'hash-set] [(multi) #'hash-update]))
+       (def/stx fn
+         #`(lambda (ignore arg ...)
+             (let ([arg (arg-p arg)] ...)
+               (lambda (acc) (hash-op acc '#,key e)))))
+       #``[(name ...) ,fn (desc #,@(stx-map identifier->string-literal #'(arg ...)))]]))
+  
   (define (compile-option option-stx key)
-    (define (compile-flags flags type)
-      (for/list ([flag (in-syntax flags)])
-        (syntax-parse flag
-          [[[name:string ...] ([arg:id arg-spec] ...) desc:string e]
-           (def/stx fn
-             (case type
-               [(choice) #`(lambda (ignore arg ...)
-                             (lambda (acc) (hash-set acc '#,key e)))]
-               [(multi) #`(lambda (ignore arg ...)
-                            (lambda (acc) (hash-update acc '#,key e)))]))
-           #``[(name ...) ,fn (desc #,@(stx-map identifier->string-literal #'(arg ...)))]])))
     (syntax-parse option-stx
       #:literal-sets (cmdline-literals)
       [(choice (~or #:required (~seq #:default default-expr)) flag ...)
-       #`(list 'once-any #,@(compile-flags #'(flag ...) 'choice))]
+       #`(list 'once-any #,@(stx-map (compile-flag key 'choice) #'(flag ...)))]
       [(multi init-expr flag ...)
-       #`(list 'multi #,@(compile-flags #'(flag ...) 'multi))]))
+       #`(list 'multi #,@(stx-map (compile-flag key 'multi) #'(flag ...)))]))
 
   (define (compile-table-expr option-specs option-keys)
     #`(list #,@(map compile-option option-specs option-keys)))
@@ -182,20 +194,10 @@
            (hash-set acc key (apply append (syntax->datum #'(flag.names ...))))]
           [_ acc])))
 
-    (define arg-parsers
-      (for/list ([t arg-types])
-        (syntax-parse t
-          #:literal-sets (cmdline-literals)
-          [simple-argument #'identity/p]
-          [(checked-argument desc parser) #'parser])))
+    (define arg-parsers (map arg-type-parser-expr arg-types))
 
     (define maybe-parse-rest
-      (if maybe-rest-type
-          (syntax-parse maybe-rest-type
-            #:literal-sets (cmdline-literals)
-            [simple-argument #'identity/p]
-            [(checked-argument desc parser) #'parser])
-          #'#f))
+      (if maybe-rest-type (arg-type-parser-expr maybe-rest-type) #'#f))
 
     (define arg-names-exprs
       (for/list ([arg arg-names])
@@ -211,7 +213,8 @@
   (define (compile-arg-help arg-names maybe-rest-name)
     #`'#,(append (map symbol->string (map syntax-e arg-names))
                  (if maybe-rest-name (list (symbol->string (syntax-e maybe-rest-name))) '())))
-  )
+
+  ) ; end compiler begin-for-syntax
 
 (define-syntax (define/command-line-options stx)
   (syntax-parse (expand-define/command-line-options stx)
@@ -219,7 +222,9 @@
         ([option-name option-spec] ...)
         ([arg-name arg-type] ...)
         (~or* #f [rest-name rest-type]))
+     
      (displayln this-syntax)
+     
      (define option-keys (for/list ([o (in-syntax #'(option-name ...))]) (gensym (syntax-e o))))
      (def/stx table-expr (compile-table-expr (syntax->list #'(option-spec ...)) option-keys))
      (def/stx finish-proc-expr
@@ -237,8 +242,7 @@
           finish-proc-expr 
           arg-help-strs-expr))])) 
 
-
-
+; sugar
 (define-option-syntax switch/o
   (syntax-parser
     [(_ flags:flag-names desc:string)
@@ -246,29 +250,29 @@
 
 (define-option-syntax list/o
   (syntax-parser
-    [(_ flags:flag-names arg:arg-spec desc:string)
+    [(_ names:flag-names arg:arg-spec desc:string)
      #'(multi '()
-              [flags arg desc (lambda (acc) (cons arg.name acc))])]))
+              [names arg desc (lambda (acc) (append acc (list arg.name)))])]))
 
-(define-flag-syntax numbered-flags/o
+(define-flag-syntax numbered-flags/f
   (syntax-parser
     [(_ flags:flag-names [min:number max:number] desc:string)
      (def/stx (f ...)
        (for/list ([n (in-range (syntax-e #'min) (syntax-e #'max))])
-         (def/stx names (map (lambda (s) (format "~a~a" s n)) (syntax->datum #'flag.names)))
+         (def/stx names (for/list ([s (syntax->datum #'flags.names)]) (format "~a~a" s n)))
          (def/stx this-desc (format "set ~a to ~a" (syntax-e #'desc) n))
-         #`[names this-desc #,(datum->syntax #'here n)]))
+         #`[names this-desc #,n]))
      #'(begin f ...)]))
 
-(define (nat-range/p min max)
+(define (int-range/p min max)
   (lambda (s)
-    (let ([n (string->number s)])
-      (unless (and (integer? n) (>= n min) (<= n max))
-        (raise-user-error (format "expected integer between ~a and ~a"
-                                  min max)))
-      n)))
-(define-argument-syntax int-range/arg
+    (define n (string->number s))
+    (unless (and (integer? n) (>= n min) (<= n max))
+      (raise-user-error (format "expected integer between ~a and ~a" min max)))
+    n))
+(define-argtype-syntax int-range/argt
   (syntax-parser
     [(_ min:number max:number)
      (def/stx desc (format "integer between ~a and ~a" (syntax-e #'min) (syntax-e #'max)))
-     #'(checked-argument desc (nat-range/p min max))]))
+     #'(checked-argument desc (int-range/p min max))]))
+
--- test1.rkt
+++ test1.rkt
@@ -6,7 +6,7 @@
 
 
 (let ()
-  (define (nat-range/p min max)
+  (define (int-range/p min max)
     (lambda (s)
       (let ([n (string->number s)])
         (unless (and (integer? n) (>= n min) (<= n max))
@@ -22,10 +22,25 @@
     #:arguments
     x
     [y simple-argument]
-    [z (checked-argument "foo" (nat-range/p 1 5))]
-    #:rest [r (checked-argument "foo" (nat-range/p 1 5))])
+    [z (checked-argument "foo" (int-range/p 1 5))]
+    #:rest [r (checked-argument "foo" (int-range/p 1 5))])
   (check-equal? x "1")
   (check-equal? y "2")
   (check-equal? z 3)
   (check-equal? r (list 4))
   (check-equal? foo "v"))
+
+(let ()
+  (define/command-line-options
+    #:argv (list "--verbose" "--lf" "a" "--lf" "b" "--optimize-level" "2")
+    #:options
+    [verbose (switch/o "--verbose" "enable verbose output")]
+    [link-flags (list/o "--lf" lf "add a link flag")]
+    [opt-level (choice #:default 0
+                       ["--optimize-level" [lvl (int-range/argt 0 3)]
+                                           "set optimization level to <lvl>" lvl]
+                       (numbered-flags/f "--o" [0 3] "optimization level"))])
+  (check-equal? verbose #t)
+  (check-equal? link-flags (list "a" "b"))
+  (check-equal? opt-level 2)
+  )
