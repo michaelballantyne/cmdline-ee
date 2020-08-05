8c8a557530952ef0a7fd193bf466644b192118f0
Michael Ballantyne <michael.ballantyne@gmail.com>
2020-08-04 21:30:17 -0600

    don't test example.rkt

--- info.rkt
+++ info.rkt
@@ -3,4 +3,5 @@
 (define name "cmdline-ee")
 (define deps '("base" "rackunit-lib" "ee-lib"))
 (define build-deps '("racket-doc" "scribble-lib"))
+(define test-omit-paths '("example.rkt"))
 ;(define scribblings '(("main.scrbl" ())))
